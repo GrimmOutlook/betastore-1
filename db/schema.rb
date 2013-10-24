25cdc0ace530be43ca28d6391ac383896ca8762d
Larry Phipps <ljphipps1@gmail.com>
2013-10-24 12:47:58 -0400

    updated mulitple tables in database, customer, password_reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011002031) do
+ActiveRecord::Schema.define(version: 20131021225539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20131011002031) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
   create_table "line_items", force: true do |t|
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20131011002031) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "subscriptions", force: true do |t|
