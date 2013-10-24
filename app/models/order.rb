feb420fa0948c926bcf2645fc50fbd5e1c844ace
Larry Phipps <ljphipps1@gmail.com>
2013-10-24 12:48:52 -0400

    Added methods when testing

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
   has_one :refund
+
+  def self.recent
+    where("placed_at > ?", 7.days.ago)
+  end
+
+  def self.by(customer)
+    where(customer_id: customer.id)
+  end
+
 end
