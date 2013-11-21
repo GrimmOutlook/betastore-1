6221b08aa475bb5704a7f2b9ab3fee970c37db2b
Larry Phipps <ljphipps1@gmail.com>
2013-11-20 22:25:21 -0500

    Added/fixed associations for order and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,15 @@
 class Product < ActiveRecord::Base
-	has_many :line_items
- 	 has_many :orders, :through => :line_items
+  has_many :line_items
+  has_many :orders, :through => :line_items
+  has_many :product_categorizations
+  has_many :categories, :through => :product_categorizations
 
-  	validates :name, presence: true
-  	validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  def self.in_category(category_id)
+    includes(:product_categorizations)
+      .where('product_categorizations.category_id' => category_id)
+  end
+
 end
