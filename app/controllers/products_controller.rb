f9e9fa713e649efdda00d277f663f3a1c9cffbfb
Larry Phipps <ljphipps1@gmail.com>
2013-11-20 22:18:48 -0500

    Added pagination to products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,45 +2,59 @@ class ProductsController < ApplicationController
 	skip_before_filter :require_log_in
 
 	def index
-		@products=Product.all
+	    @categories = Category.all
 
-		respond_to do |format|
-			format.html
-			format.rss
-			format.json do
-				render json: @products
-			end
-		end
+	    scope = Product
+
+	    if params[:min_price].present?
+	      scope = scope.where('price >= ?', params[:min_price])
+	    end
+
+	    if params[:max_price].present?
+	      scope = scope.where('price <= ?', params[:min_price])
+	    end
+
+	    if params[:category_id].present?
+	      scope = scope.in_category(params[:category_id])
+	    end
+
+	    @products = scope.page(params[:page]).per(12)
+
+	    respond_to do |format|
+	      format.html
+	      format.rss
+	    end
 	end
 
-	def create
-		@product = Product.new(product_params)
+	# def create
+	# 	@product = Product.new(product_params)
 
-		respond_to do |format|
-			if @product.save
-				format.html do
-					redirect_to product_path(@product), notice: 'Product was saved'
-				end
-				format.json do
-					render json: @product
-				end
-			else
-				format.html do
-					render 'edit'
-				end
-				format.json do
-					render json: { errors: @product.errors}, status: 422
-				end
-			end
-		end
-	end
+	# 	respond_to do |format|
+	# 		if @product.save
+	# 			format.html do
+	# 				redirect_to product_path(@product), notice: 'Product was saved'
+	# 			end
+	# 			format.json do
+	# 				render json: @product
+	# 			end
+	# 		else
+	# 			format.html do
+	# 				render 'edit'
+	# 			end
+	# 			format.json do
+	# 				render json: { errors: @product.errors}, status: 422
+	# 			end
+	# 		end
+	# 	end
+	# end
 
 	def show
+		@product = Product.find(params[:id])
 	end
 
-		private
-		def product_params
-			params.require(:product).permit(:name, :price)
-		end
+	# private
+	# def product_params
+	# 	params.require(:product).permit(:name, :price)
+	# end
 
 end
